13cc7db05e0d857b4372297bf2af2acead8b95ac
Ismael Perez <ismael.jouhari@gmail.com>
2024-06-03 18:33:36 +0200

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'chatrooms/index'
-  get 'chatrooms/show'
-  get 'movies/index'
-  get 'movies/show'
-  get 'recommendations/index'
-  get 'lists/show'
   devise_for :users
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,5 +7,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "pages#home"
+  resources :users, only: :show
+  resources :recommendations, only: :index
+  resources :lists, only: :show
+  resources :movies, only: %i[index show] do
+    resources :list_movies, only: %i[create]
+  end
+  resources :chatrooms
 end
