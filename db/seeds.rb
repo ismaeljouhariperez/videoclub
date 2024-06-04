6eedee57d768b83e40077d7b18be1fb16b257f0d
Ismael Perez <ismael.jouhari@gmail.com>
2024-06-04 11:59:50 +0200

    Seed basic setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require 'net/http'
 require 'json'
 
 User.destroy_all if Rails.env.development?
-Watched.destroy_all if Rails.env.development?
+# Watched.destroy_all if Rails.env.development?
 List.destroy_all if Rails.env.development?
 ListMovie.destroy_all if Rails.env.development?
 Movie.destroy_all if Rails.env.development?
@@ -11,7 +11,6 @@ Movie.destroy_all if Rails.env.development?
 User.create(email: 'ismael@test.com', password: 'azerty')
 User.create(email: 'killian@test.com', password: 'azerty')
 User.create(email: 'benoit@test.com', password: 'azerty')
-
 
 url = URI.parse('https://tmdb.lewagon.com/movie/top_rated')
 response = Net::HTTP.get_response(url)
@@ -24,6 +23,6 @@ data['results'].each do |movie|
     year: movie['release_date'].split('-').first,
     actors: Faker::Movies::HarryPotter.character,
     director: Faker::Movies::HarryPotter.character,
-    # poster_url: "https://image.tmdb.org/t/p/w1280#{movie['poster_path']}",
+    poster_url: "https://image.tmdb.org/t/p/w1280#{movie['poster_path']}",
   )
 end
