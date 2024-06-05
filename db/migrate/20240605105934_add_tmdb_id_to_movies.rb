d1f625028c3ba7d69b542e6c485b6295b6edc6ef
Killian Portier <killian.portier@yahoo.fr>
2024-06-05 18:13:30 +0200

    delete migration

--- db/migrate/20240605105934_add_tmdb_id_to_movies.rb
+++
@@ -1,5 +0,0 @@
-class AddTmdbIdToMovies < ActiveRecord::Migration[7.1]
-  def change
-    add_column :movies, :tmdb_id, :integer, null: false
-  end
-end
--- db/migrate/20240605121050_delete_tmdb_id_to_movies.rb
+++
@@ -1,5 +0,0 @@
-class DeleteTmdbIdToMovies < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :movies, :tmdb_id, :integer
-  end
-end
