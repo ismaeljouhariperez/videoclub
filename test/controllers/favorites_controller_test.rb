570d562e44bbe19ff74c3ad0b8d64e94e64302b7
Ismael Perez <ismael.jouhari@gmail.com>
2024-06-06 14:56:34 +0200

    Added Favorites

---
+++ test/controllers/favorites_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class FavoritesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get favorites_index_url
+    assert_response :success
+  end
+end
