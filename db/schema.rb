46fb9cf5e6e1fd741ad39d85ef400d933787dfe8
Killian Portier <killian.portier@yahoo.fr>
2024-06-06 12:43:30 +0200

    create the modal with trailer and add trailer key to Movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_05_121119) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_06_102444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_05_121119) do
     t.datetime "updated_at", null: false
     t.string "poster_url"
     t.string "imdb_id"
+    t.string "trailer_key"
   end
 
   create_table "users", force: :cascade do |t|
