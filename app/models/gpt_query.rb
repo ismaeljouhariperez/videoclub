8f9c2b8d39220fb403d3aa104fb29d70c601c53f
Benoit GILLIOT <b.gilliot@gmail.com>
2024-06-07 12:20:57 +0200

    refacto recommandation controller

--- app/models/gpt_query.rb
+++ app/models/gpt_query.rb
@@ -2,4 +2,14 @@ class GptQuery < ApplicationRecord
   belongs_to :user
   has_many :query_movies, dependent: :destroy
   has_many :movies, through: :query_movies
+
+  after_create :create_query_movies
+
+  def create_query_movies
+    ids = ChatGptService.new.get_imdb_ids(self.query)
+    ids.each do |id|
+      movie = Movie.custom_find_or_create_by_imndb_id(id)
+      QueryMovie.create(movie: movie, gpt_query: self)
+    end
+  end
 end
