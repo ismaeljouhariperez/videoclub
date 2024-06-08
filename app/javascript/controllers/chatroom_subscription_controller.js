f38f14c4f9334a79027feb32712cd7a455ac9dc5
Killian Portier <killian.portier@yahoo.fr>
2024-06-08 04:42:50 +0200

    add some css on the messages

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -6,6 +6,9 @@ export default class extends Controller {
   static targets = ["messages"]
 
   connect() {
+    // Scroll to the bottom of the messages container
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+    // Subscribe to the chatroom channel
     this.subscription = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
       { received: data => this.#insertMessageAndScrollDown(data) }
@@ -13,10 +16,12 @@ export default class extends Controller {
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 
+  // Clean the form after submitting a new message
   resetForm(event) {
     event.target.reset()
   }
 
+  // Private method for inserting a new message and scrolling down
   #insertMessageAndScrollDown(data) {
     this.messagesTarget.insertAdjacentHTML('beforeend', data)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
