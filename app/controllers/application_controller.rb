4aee26bf8bc23bd13bf1e4b523c5dba4caccfe2c
Killian Portier <killian.portier@yahoo.fr>
2024-06-09 01:57:33 +0200

    add set lists application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_list_sidebar
+  before_action :set_lists
 
 
   def set_list_sidebar
     @lists_sidebar = List.where(user: current_user).order(updated_at: :desc).first(5)
     @list = List.new
+  end
+
+  def set_lists
+    @lists = List.where(user: current_user).order(updated_at: :desc)
   end
 end
