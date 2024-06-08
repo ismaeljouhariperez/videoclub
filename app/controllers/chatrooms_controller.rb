b2681c0fbd6a42d372c3f8c66bec53d6285a2376
Killian Portier <killian.portier@yahoo.fr>
2024-06-08 16:25:45 +0200

    can create and delete chatroom if it's your creation

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,10 +1,34 @@
 class ChatroomsController < ApplicationController
   def index
     @chatrooms = Chatroom.all
+    @chatroom = Chatroom.new
   end
 
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    @chatroom.user = current_user
+    if @chatroom.save
+      redirect_to chatrooms_path
+    else
+      @chatrooms = Chatroom.all
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @chatroom = Chatroom.find(params[:id])
+    @chatroom.destroy
+    redirect_to chatrooms_path
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:name)
   end
 end
