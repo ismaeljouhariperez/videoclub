ddde338f0f05a182c2aab6022e036bd99a490182
Ismael Perez <ismael.jouhari@gmail.com>
2024-06-08 16:24:04 +0200

    All buttons working, last step : adding a movie to a selected list

--- app/javascript/controllers/movie_buttons_controller.js
+++ app/javascript/controllers/movie_buttons_controller.js
@@ -1,8 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "buttons" ]
   static values = { movieId: Number, movieSection: String }
+
+  static targets = ["AddToWatched", "AddToWatchLater", "AddToFavorites"]
+
 
   connect() {
   }
@@ -27,8 +29,7 @@ export default class extends Controller {
     })
     .then(data => {
       console.log('Success:', data);
-      this.updateCard(data, action, section);
-      el.classList.toggle(action);
+      this.updateCard(data, action, section, el);
     })
     .catch(error => console.error('Error:', error));
   }
@@ -47,8 +48,6 @@ export default class extends Controller {
 
   addToFavorites(e) {
     const el = e.currentTarget
-    // console.log(this.buttonsTarget)
-    const favoriteButton = this.buttonsTarget.querySelector('i.favorite');
     const url = `/favorites`;
     this.fetchMovie('', 'favorite', url, el);
   }
@@ -57,35 +56,26 @@ export default class extends Controller {
     // console.log("Added to List!")
   }
 
-  // updateCard(data, action) {
-  //   // console.log(data.section, action);
-  //   const cardElement = this.element;
-  //   if ((data.section === 'watched' || data.section === 'watch_later') && data.section !== action) {
-  //     cardElement.remove();
-  //   }
-  // }
+  updateCard(data, action, section, el) {
+    console.log("data:", data);
+    console.log(action);
 
-  updateCard(data, action, section) {
-    console.log("data: ", data);
-    console.log("action: ", action);
-    console.log("section: ", section);
     const cardElement = this.element;
-
-    if (section !== "" || section !== undefined || section !== null) {
-      if ( (data.message === 'Movie watch status removed' || data.message === 'Movie changed as watched' || data.message === 'Movie changed as watch later') && (section === 'watched' || section === 'watch_later')){
-        cardElement.remove();
-      }
+    el.classList.toggle(action);
+    if (data.is_watched === true) {
+      this.AddToWatchLaterTarget.classList.remove('watch_later');
+    } else {
+      this.AddToWatchedTarget.classList.remove('watched');
     }
-    else {
-      const watchIcon = cardElement.querySelector('.fa-eye');
-      const watchLaterIcon = cardElement.querySelector('.fa-clock');
-      if (action === 'watched') {
-        watchIcon.classList.add('watched');
-        watchLaterIcon.classList.remove('watch_later');
-        }
-      else {
-        watchIcon.classList.remove('watched');
-        watchLaterIcon.classList.add('watch_later');
+
+    if (section !== "" && section !== undefined && section !== null) {
+      console.log('Section found');
+
+      if ( (data.message === 'Movie watch status removed' || data.message === 'Movie changed as watched' || data.message === 'Movie changed as watch later' || data.message === 'Movie removed from favorites')) {
+        cardElement.classList.add('fade');
+        setTimeout(() => {
+          cardElement.remove();
+        }, 700);
       }
     }
   }
