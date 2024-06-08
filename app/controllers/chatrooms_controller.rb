5b921f430cc29f340a1b9f0e4325711bc97994c0
Killian Portier <killian.portier@yahoo.fr>
2024-06-08 03:35:24 +0200

    now we can create chatroom and messages

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -4,5 +4,7 @@ class ChatroomsController < ApplicationController
   end
 
   def show
+    @chatroom = Chatroom.find(params[:id])
+    @message = Message.new
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,22 @@
 class MessagesController < ApplicationController
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.chatroom = @chatroom
+    @message.user = current_user
+    if @message.save
+      ActionCable.server.broadcast("chatroom_#{params[:chatroom_id]}",
+        render_to_string(partial: "chatrooms/message", locals: { message: @message })
+      )
+      head :ok
+    else
+      render "chatrooms/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
