e54221097e702ce74e189a4f93e5106fe189ca62
Killian Portier <killian.portier@yahoo.fr>
2024-06-10 15:38:15 +0200

    minor change style

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -31,7 +31,7 @@ export default class extends Controller {
 
   #buildMessageElement(currentUserIsSender, message) {
     return `
-      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+      <div class="d-flex ${this.#justifyClass(currentUserIsSender)}">
         <div class="${this.#userStyleClass(currentUserIsSender)}">
           ${message}
         </div>
