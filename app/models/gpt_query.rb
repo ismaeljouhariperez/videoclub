9ccf3ec6d6b311d8a3a5b38dce2b0628bf14e398
Benoit GILLIOT <b.gilliot@gmail.com>
2024-06-12 16:37:37 +0200

    back for reco_show

--- app/models/gpt_query.rb
+++ app/models/gpt_query.rb
@@ -8,11 +8,23 @@ class GptQuery < ApplicationRecord
   after_create :create_query_movies
 
   def create_query_movies
-    JSON.parse(ChatGptService.new.get_recommendations(self)).each do |reco|
+    response = ChatGptService.new.get_recommendations(self)
+
+    # Extract JSON part from the response
+    json_start = response.index('[')
+    json_end = response.rindex(']') + 1
+    json_string = response[json_start...json_end]
+
+    # Parse the extracted JSON string
+    JSON.parse(json_string).each do |reco|
       movie_id = reco["imdb_id"]
       movie = Movie.custom_find_or_create_by_imndb_id(movie_id)
       QueryMovie.create(movie: movie, reco_title: reco["title"], reco_description: reco["description"], gpt_query: self)
     end
+    
+  rescue JSON::ParserError => e
+    Rails.logger.error "Failed to parse JSON: #{e.message}"
+    # Handle the error appropriately, e.g., by notifying the user or re-attempting the request
   end
 
   def associated_movies
